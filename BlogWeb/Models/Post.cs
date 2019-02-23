937bd015b54a21548e8f303a6f8a63b1a4956f3b
fn23caelum@gmail.com <fn23caelum@gmail.com>
2019-02-23 10:04:34 -0300

    7.2

--- BlogWeb/Models/Post.cs
+++ BlogWeb/Models/Post.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlogWeb.Validacoes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,9 +10,11 @@ namespace BlogWeb.Models
     public class Post
     {
         public int Id { get; set; }
-        [Required][StringLength(50)]
+        [Required(ErrorMessage = "Campo requirido")]
+        [StringLength(50)]
+        [FilmeRambo]
         public string Titulo { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Campo requirido")]
         public string Resumo { get; set; }
         public string Categoria { get; set; }
         public DateTime? DataPublicacao { get; set; }
---
+++ BlogWeb/Validacoes/FilmeRamboAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogWeb.Validacoes
+{
+    public class FilmeRamboAttribute : ValidationAttribute
+    {
+        public FilmeRamboAttribute() : base("Fora Rambo")
+        {
+
+        }
+
+        public override bool IsValid(object titulo) => titulo != null ? titulo.ToString() == "Rambo" ?  false :  true : false;
+    }
+}
